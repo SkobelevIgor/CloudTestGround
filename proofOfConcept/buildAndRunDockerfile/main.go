1f4637e9504eedc92d74caf6633dd3936a85b76d
igor <skobelev.igor@gmail.com>
2018-11-21 00:25:10 +0200

    container remove

--- proofOfConcept/buildAndRunDockerfile/main.go
+++ proofOfConcept/buildAndRunDockerfile/main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"io"
 	"log"
 	"os"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/client"
 	"github.com/docker/docker/pkg/archive"
 )
@@ -21,7 +21,10 @@ func main() {
 		panic(err)
 	}
 
-	containerInfo(ctx, cli, "test")
+	cDel := containersToDelete(ctx, cli, "test")
+	deleteContainers(ctx, cli, cDel)
+	// iDel := imagesToDelete(ctx, cli, "test")
+	// deleteImages(iDel)
 
 	// buildImage(ctx, cli, "test")
 	// id := runImage(ctx, cli, "test")
@@ -29,19 +32,58 @@ func main() {
 	// fmt.Println(id)
 }
 
-func containerInfo(ctx context.Context, cli *client.Client, imageTag string) {
-
-	cl, err := cli.ContainerList(ctx, types.ContainerListOptions{})
+func containersToDelete(ctx context.Context, cli *client.Client, imageTag string) []string {
+	filter := filters.NewArgs()
+	filter.Add("name", imageTag)
+	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{Filters: filter})
 	if err != nil {
 		panic(err)
 	}
 
-	for _, ctr := range cl {
-		fmt.Println(ctr.Image)
-		fmt.Println("\n ------------------------------------------- ")
+	var cIds []string
+	for _, ctr := range containers {
+		cIds = append(cIds, ctr.ID)
 	}
 
+	return cIds
 }
+
+func imagesToDelete(ctx context.Context, cli *client.Client, imageTag string) []string {
+
+	images, err := cli.ImageList(ctx, types.ImageListOptions{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	var iIds []string
+
+	for _, img := range images {
+		for _, tag := range img.RepoTags {
+			if tag == imageTag+":latest" {
+				iIds = append(iIds, img.ID[7:])
+			}
+		}
+	}
+
+	return iIds
+
+}
+
+func deleteContainers(ctx context.Context, cli *client.Client, cIds []string) (st bool) {
+	status := true
+	for _, cID := range cIds {
+		err := cli.ContainerRemove(ctx, cID, types.ContainerRemoveOptions{Force: true})
+		if err != nil {
+			status = false
+		}
+	}
+	return status
+}
+
+// func deleteImages(iIds []string) (st bool) {
+
+// }
 
 func getContext(filePath string) io.Reader {
 	ctx, _ := archive.TarWithOptions(filePath, &archive.TarOptions{})
@@ -70,7 +112,8 @@ func runImage(ctx context.Context, cli *client.Client, imageTag string) string {
 	resp, err := cli.ContainerCreate(ctx, &container.Config{
 		Image: imageTag,
 		Tty:   false,
-	}, nil, nil, "")
+	}, &container.HostConfig{
+		AutoRemove: true}, nil, "ctg_"+imageTag)
 	if err != nil {
 		panic(err)
 	}
