e5e3619981bebd3f7a4fbfcd04649176477d1ba4
igor <skobelev.igor@gmail.com>
2018-11-23 00:34:13 +0200

    init script

--- proofOfConcept/awsApi/ec2Init.sh
+++ proofOfConcept/awsApi/ec2Init.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
 
-sudo yum install -y git
-echo 'done' > /home/ec2-user/file
+wget https://raw.githubusercontent.com/SkobelevIgor/CloudTestGround/dev/proofOfConcept/awsApi/server/server.go
+
+sudo yum install -y go
+
+echo "export GOPATH=~" >> ~/.bash_profile
+echo "export GOBIN=~/bin" >> ~/.bash_profile
+export GOPATH=~
+export GOBIN=~/bin
+
+go install server.go
+
+cat << EOF >> /lib/systemd/system/server-agent.service
+[Unit]
+Description=server-agent
+
+[Service]
+Type=simple
+Restart=always
+RestartSec=5s
+ExecStart=$GOBIN/server
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+service server-agent start
+service server-agent enable
+
+
+
